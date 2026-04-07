296a0eed7d3b3b5eb454b2c87e6225bed1ab4e46
treacle2 <2504040222@ogr.kent.edu.tr>
2026-04-08 02:09:50 +0300

    Applied force to the Player(Ball).

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private float movementX;
+    private float movementY;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,10 +15,13 @@ public class PlayerController : MonoBehaviour
     void OnMove (InputValue movementValue)
     {
         Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
     }
 
     void FixedUpdate() 
     {
-        
+     Vector3 movement = new Vector3 (movementX, 0.0f, movementY);   
+     rb.AddForce(movement);   
     }
 }
