ea38df0762b28484e7eb56a1a1d0ce608906a625
treacle2 <2504040222@ogr.kent.edu.tr>
2026-04-08 14:39:36 +0300

    Pickups can be collected .
    and there is a counter

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,29 +1,96 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
+
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 0;
+ // Rigidbody of the player.
+ private Rigidbody rb; 
 
-    private Rigidbody rb;
-    private float movementX;
-    private float movementY;
+ // Variable to keep track of collected "pickup" objects.
+ private int count;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+ // Movement along X and Y axes.
+ private float movementX;
+ private float movementY;
+
+ // Speed at which the player moves.
+ public float speed = 0;
+
+ // UI text component to display count of "pickup" objects collected.
+ public TextMeshProUGUI countText;
+
+ // UI object to display winning text.
+ public GameObject winTextObject;
+
+ // Start is called before the first frame update.
+ void Start()
     {
-    rb = GetComponent <Rigidbody>();    
+ // Get and store the Rigidbody component attached to the player.
+        rb = GetComponent<Rigidbody>();
+
+ // Initialize count to zero.
+        count = 0;
+
+ // Update the count display.
+        SetCountText();
+
+ // Initially set the win text to be inactive.
+        winTextObject.SetActive(false);
     }
-
-    void OnMove (InputValue movementValue)
+ 
+ // This function is called when a move input is detected.
+ void OnMove(InputValue movementValue)
     {
+ // Convert the input value into a Vector2 for movement.
         Vector2 movementVector = movementValue.Get<Vector2>();
+
+ // Store the X and Y components of the movement.
         movementX = movementVector.x; 
         movementY = movementVector.y; 
     }
 
-    void FixedUpdate() 
+ // FixedUpdate is called once per fixed frame-rate frame.
+ void FixedUpdate() 
     {
-     Vector3 movement = new Vector3 (movementX, 0.0f, movementY);   
-     rb.AddForce(movement * speed);   
+ // Create a 3D movement vector using the X and Y inputs.
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+
+ // Apply force to the Rigidbody to move the player.
+        rb.AddForce(movement * speed); 
+    }
+
+ 
+ void OnTriggerEnter(Collider other) 
+    {
+ // Check if the object the player collided with has the "pickup" tag.
+ if (other.gameObject.CompareTag("pickup")) 
+        {
+ // Deactivate the collided object (making it disappear).
+            other.gameObject.SetActive(false);
+
+ // Increment the count of "pickup" objects collected.
+            count = count + 1;
+
+ // Update the count display.
+            SetCountText();
+        }
+    }
+
+ // Function to update the displayed count of "pickup" objects collected.
+ void SetCountText() 
+    {
+ // Update the count text with the current count.
+        countText.text = "Count: " + count.ToString();
+
+ // Check if the count has reached or exceeded the win condition.
+ if (count >= 12)
+        {
+ // Display the win text.
+            winTextObject.SetActive(true);
+        }
     }
 }
--- README.md
+++ README.md
@@ -12,3 +12,5 @@ Applied input data to the Player(The Ball).
 The ball moves now and the camera follows the ball.
 Walls added to the playing surface.
 Pickups added but it is just rotating not interactable.
+Pickups are now interactable.
+When you collect the pickups there is a counter on the top left of the screen that counts.
